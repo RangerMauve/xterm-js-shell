2f999e5954cf839fe02872ffb6eeb0887b7b88f2
rangermauve <RangerMauve@hotmail.com>
2019-04-14 21:13:41 -0400

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # xterm-js-shell
 Building block for CLI environments for xterm.js
+
+```js
+const XtermJSShell = require('xterm-js-shell')
+
+const terminal = new Terminal()
+
+const shell = new XtermJSShell(terminal)
+
+shell
+  .command('help', (shell) => {
+    await shell.printLine(`
+Try running one of these commands:
+- fetch
+- echo
+- confirm
+`)
+  })
+  .command('fetch', (shell, [ url ]) => {
+    const response = await fetch(url)
+
+    const text = await response.text()
+
+    await shell.print(text)
+  })
+  .command('echo', (shell) => {
+    let message = null
+    // Loop until they hit enter without typing anything
+    while(message = await shell.readLine('> ')) {
+      await shell.printLine(message)
+    }
+  })
+  .command('confirm', (shell) => {
+    const char = await shell.readChar('Y/n?')
+
+    await shell.printLine(char)
+  })
+
+// Start the Read-Eval-Print-Loop
+shell.repl()
+
+terminal.open(someElement)
+```
+
+Features:
+
+- Takes an [`xterm.js` terminal](https://xtermjs.org/) instance
+- Keyboard navigation, history, tab completion [using local-echo](https://github.com/wavesoft/local-echo)
+- Able to define commands with a name and `async` function
+- Commands take a `shell` instance, and `args` array
+- Commands should be able to `readline`, `readchar`, `printline`, and `print`
+- Commands should be able to `run` other commands
+- After a command resolves or rejects, it's `shell` should throw whenever there's input
+- Commands should be able to "take over" the terminal to prevent default processing
+  - Useful for stuff like SSH
+  - Shell will take back control after the program exits
+- Able to `detach` shell from the terminal
---
+++ index.js
@@ -0,0 +1,174 @@
+const LocalEchoController = require('local-echo')
+const chalk = require('chalk')
+
+const ERROR_NOT_FOUND = (command) => `Command Not Found: ${command}`
+const ERROR_ALREADY_REGISTERED = (command) => `Command Already Registered: ${command}`
+
+const WHITESPACE_REGEX = /[\s\r\n]+/g
+const RESET = chalk.reset
+
+/**
+ * Command structure
+ * @callback Command
+ * @param {SubShell} shell Shell instance for input/output
+ * @param {Array<string>} args Arguments for the command
+ */
+
+module.exports =
+
+/** Shell abstraction for Xterm.js */
+class XtermJSShell {
+  /**
+   * Instantiate and attach a shell to the terminal
+   * @param {Terminal} term The xterm.js terminal
+   */
+  constructor (term) {
+    this.prompt = `${this.color.yellow}> ${RESET}`
+    this.commands = new Map()
+    this.echo = new LocalEchoController(term)
+
+    this.attached = true
+
+    this.echo.addAutocompleteHandler(this.autoCompleteCommands.bind(this))
+  }
+
+  /**
+   * Detach the shell from xtermjs
+   */
+  detach () {
+    if (!this.attached) return
+    this.echo.detach()
+    this.attached = false
+  }
+
+  /**
+   * Attach the shell to the terminal
+   */
+  attach () {
+    if (this.attached) return
+    this.echo.attach()
+    this.attached = true
+  }
+
+  /**
+   * Utility for getting
+   * @return {chalk} A [chalk](https://www.npmjs.com/package/chalk) instance
+   */
+  get color () {
+    return chalk
+  }
+
+  /**
+   * Read-eval-print-loop, run this to start the shell
+   * @return {Promise} Resolves after a pass of the loop finishes
+   */
+  async repl () {
+    // Read
+    const line = await this.echo.read(this.prompt)
+
+    const [command, ...args] = line.split(WHITESPACE_REGEX)
+
+    try {
+      // Eval / Print
+      await this.run(command, args)
+    } catch (e) {
+      console.error(e)
+      await this.echo.println(e.message)
+    }
+
+    // Loop
+    this.repl()
+  }
+
+  /**
+   * Run a command in the shell
+   * @param  {string}         command The name of the command to run
+   * @param  {Array<string>}  args    The list of command arguments to run
+   * @return {Promise}                Resolves after the command has finished
+   */
+  async run (command, args) {
+    const fn = this.commands.get(command)
+
+    if (!fn) return new TypeError(ERROR_NOT_FOUND(command))
+
+    const shell = new SubShell(this)
+
+    await fn(shell, args)
+
+    shell.destroy()
+  }
+
+  /**
+   * Add a command to the shell
+   * @param  {string}        command The name of the command
+   * @param  {Command}      fn      Async function that takes a shell / args
+   * @return {XtermJSShell}          Returns self for chaining
+   */
+  command (command, fn) {
+    if (this.commands.has(command)) {
+      console.warn(ERROR_ALREADY_REGISTERED(command))
+    }
+
+    this.commands.set(command, fn)
+
+    return this
+  }
+
+  // Internal command for auto completion of command names
+  autoCompleteCommands (index, tokens) {
+    if (index === 0) {
+      return [...this.commands.values()]
+    } else {
+      return []
+    }
+  }
+}
+
+class SubShell {
+  constructor (shell) {
+    this.shell = shell
+    this.destroyed = false
+  }
+
+  async readChar (message) {
+    this.checkDestroyed()
+    return this.shell.readChar(message)
+  }
+
+  async readLine (message) {
+    this.checkDestroyed()
+    return this.shell.read(message)
+  }
+
+  async abortRead (reason) {
+    this.checkDestroyed()
+    return this.shell.abortRead(reason)
+  }
+
+  async print (message) {
+    this.checkDestroyed()
+    this.shell.print(message)
+  }
+
+  async printLine (message) {
+    this.checkDestroyed()
+    this.shell.println(message)
+  }
+
+  async printList (list) {
+    this.checkDestroyed()
+    this.shell.printWide(list)
+  }
+
+  get color () {
+    return chalk
+  }
+
+  checkDestroyed () {
+    if (this.destroyed) throw new Error('Terminal destroyed')
+  }
+
+  destroy () {
+    this.destroyed = true
+  }
+}
